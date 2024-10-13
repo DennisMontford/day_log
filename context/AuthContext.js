88e1da6cdc57a6634fb6e3840f1b018e2e11eb4c
Dennis <draymontford@gmail.com>
2024-10-13 18:08:54 -0500

    full code

---
+++ context/AuthContext.js
@@ -0,0 +1,79 @@
+'use client'
+import { auth, db } from '@/firebase'
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import { doc, getDoc } from 'firebase/firestore'
+import React, {useContext, useState, useEffect} from 'react'
+
+
+const AuthContext = React.createContext()
+
+export function useAuth(){
+    return useContext(AuthContext)
+}
+
+export function AuthProvider({children}) {
+    const [currentUser, setCurrentUser] = useState(null)
+    const [userDataObj, setUserDataObj] = useState(null)
+    const [loading, setLoading] = useState(true)
+  
+    // HANDLE AUTH
+    function signup(email, password) {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    function login(email, password) {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    function logout() {
+        setUserDataObj(null)
+        setCurrentUser(null)
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, async user => {
+            try {
+                //SET USER TO LOCAL CONT ST
+                setLoading(true)
+                setCurrentUser(user)
+                if (!user) {
+                    console.log('No User Found')
+                    return
+                }
+
+                // IF USER, GET DATA FROM FSDB
+                console.log('Fetching User Data')
+                const docRef = doc(db, 'users', user.uid)
+                const docSnap = await getDoc(docRef)
+                let firebaseData = {}
+                if (docSnap.exists()) {
+                    console.log('Found User Data')
+                    firebaseData = docSnap.data()
+                }
+                setUserDataObj(firebaseData)
+            } catch(err) {
+                console.log(err.message)
+            } finally {
+                setLoading(false)
+            }
+        })
+      return unsubscribe
+    }, [])
+
+    const value = {
+        currentUser,
+        userDataObj,
+        setUserDataObj,
+        signup,
+        logout,
+        login,
+        loading
+    }
+
+    return(
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
